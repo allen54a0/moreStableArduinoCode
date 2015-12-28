6394805eb2d441103f4ab36292b1c5accdc19c40
allen54a0 <allen54a0@gmail.com>
2015-12-29 00:06:04 +0800

    Update DebugMacro.ino

--- DebugMacro.ino
+++ DebugMacro.ino
@@ -1,4 +1,19 @@
-#define DEBUG 1
+/*  DedugMacro.ino
+ *   除錯訊息開關範例
+ *   使用時機 : 部分除錯訊息不想在正式執行輸出，可以用這個小技巧
+ *   使用方法 :  設定  " #define DEBUG X "  , X ==0  不印出訊息，X==1 印出訊息    
+ *   
+ *   引用來源 : C 教科書衍生
+ *      
+ *   Author : allen54a0
+ *   DATE: 2015 1229
+ * 
+ */
+
+
+
+
+#define DEBUG 1         // 藉由定義 0 或 1 來關閉或開啟除錯訊息
 #define DEBUGP(x) if (DEBUG == 1) { x ; }
 
 
@@ -12,17 +27,17 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  //  
 
 
-  //do something
+  //do something, 一般的程式操作
 
 
 
-  //Normal Dump
+  //Normal Dump ,一般需要輸出的訊息
   Serial.println("Status DUMP");
 
-  //dump debug Message 
+  //dump debug Message  , 僅有除錯時要印出輸出的訊息，正式執行時不輸出
   DEBUGP(Serial.println("DEBUG  DUMP"));
 
  
